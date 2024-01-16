76bc10b0153905da8052fbba3df1261e179b8059
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-17 00:01:46 +0100

    registered user model to sequelize

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { User } from './entities/user.entity';
 import { AccountService } from './account.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AccountController } from './account.controller';
 
 @Module({
-  controllers: [AccountController],
   providers: [AccountService],
+  controllers: [AccountController],
+  imports: [SequelizeModule.forFeature([User])],
 })
-export class AccountModule {}
+export class AccountModule { }
