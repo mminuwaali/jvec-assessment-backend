392fb745a9a2a0d6eec2c870a854d9569de04f3c
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-17 00:40:41 +0100

    updated the contact controllers

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -1,34 +1,36 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ContactService } from './contact.service';
+import { IsAuthenticatedGuard } from 'src/app.guards';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
+import { Controller, Get, Post, Req, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 
-@Controller('contact')
+@Controller('api')
+@UseGuards(IsAuthenticatedGuard)
 export class ContactController {
-  constructor(private readonly contactService: ContactService) {}
+  constructor(private readonly contactService: ContactService) { }
 
-  @Post()
-  create(@Body() createContactDto: CreateContactDto) {
-    return this.contactService.create(createContactDto);
+  @Get('contact')
+  findAll(@Req() req: RequestType) {
+    return this.contactService.findAll(+req.user.id);
   }
 
-  @Get()
-  findAll() {
-    return this.contactService.findAll();
+  @Get('contact/:id')
+  findOne(@Param('id') id: string, @Req() req: RequestType) {
+    return this.contactService.findOne(+id, +req.user.id);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.contactService.findOne(+id);
+  @Delete('contact/:id')
+  remove(@Param('id') id: string, @Req() req: RequestType) {
+    return this.contactService.remove(+id, +req.user.id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateContactDto: UpdateContactDto) {
-    return this.contactService.update(+id, updateContactDto);
+  @Post('contact')
+  create(@Body() createContactDto: CreateContactDto, @Req() req: RequestType) {
+    return this.contactService.create(createContactDto, +req.user.id);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.contactService.remove(+id);
+  @Patch('contact/:id')
+  update(@Param('id') id: string, @Body() updateContactDto: UpdateContactDto, @Req() req: RequestType) {
+    return this.contactService.update(+id, updateContactDto, +req.user.id);
   }
 }
