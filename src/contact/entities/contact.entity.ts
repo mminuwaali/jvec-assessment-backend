0ac57b5a5d30157fbbdca3e4d0b8a115e2c57edc
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-17 00:39:46 +0100

    created the contact model

--- src/contact/entities/contact.entity.ts
+++ src/contact/entities/contact.entity.ts
@@ -1,1 +1,21 @@
-export class Contact {}
+import { User } from 'src/account/entities/user.entity';
+import { Model, Table, Column, DataType, BeforeCreate, AfterCreate, ForeignKey, BelongsTo } from 'sequelize-typescript';
+
+@Table
+export class Contact extends Model<Contact> {
+    @Column
+    firstName: string;
+
+    @Column
+    lastName: string;
+
+    @Column
+    phoneNumber: string;
+
+    @ForeignKey(() => User)
+    userId: number;
+
+    @BelongsTo(() => User, { onDelete: 'CASCADE' })
+    user: User;
+
+};
