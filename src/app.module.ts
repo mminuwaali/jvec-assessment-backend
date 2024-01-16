9748630ae401df4d7f1b03a0d8181f545f394804
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-16 19:33:49 +0100

    configured jwt for authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  providers: [],
   controllers: [],
+  providers: [ConfigService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    JwtModule.registerAsync({ global: true, inject: [ConfigService], useFactory: (service: ConfigService) => ({ secret: service.get('SECRET_KEY') }) }),
+  ],
 })
 export class AppModule { }
