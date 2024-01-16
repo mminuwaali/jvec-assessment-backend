d013eafe6d3f69249bf1073f8e2b2d0b99017d48
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-17 00:02:46 +0100

    setup the authentication endpoints

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { LoginDTO } from './dto/login.dto';
+import { RegisterDTO } from './dto/register.dto';
 import { AccountService } from './account.service';
+import { Body, Controller, Post } from '@nestjs/common';
 
-@Controller('account')
+@Controller('api')
 export class AccountController {
-  constructor(private readonly accountService: AccountService) {}
+  constructor(private readonly accountService: AccountService) { }
+
+  @Post('login')
+  async login(@Body() body: LoginDTO) {
+    return this.accountService.login(body);
+  }
+
+  @Post('register')
+  async register(@Body() body: RegisterDTO) {
+    return this.accountService.register(body);
+  }
 }
