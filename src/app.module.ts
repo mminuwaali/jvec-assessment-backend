64ba2c61e8ad50d47959a555a0cfcd3a4c36a302
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-17 00:04:11 +0100

    replaced the jwt module with the JWTModule module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
-import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JWTModule } from './jwt.module';
+import { DatabaseModule } from './db.module';
 import { AccountModule } from './account/account.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [],
   providers: [ConfigService],
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    JwtModule.registerAsync({ global: true, inject: [ConfigService], useFactory: (service: ConfigService) => ({ secret: service.get('SECRET_KEY') }) }),
+    JWTModule,
     AccountModule,
+    DatabaseModule,
+    ConfigModule.forRoot({ isGlobal: true }),
   ],
 })
 export class AppModule { }
