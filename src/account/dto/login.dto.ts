0b4248d246b95fbece505dc1db1ede755dc5ddbf
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-19 01:14:36 +0100

    setup login and register docs

--- src/account/dto/login.dto.ts
+++ src/account/dto/login.dto.ts
@@ -1,15 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsEmail, MinLength, ValidateIf } from 'class-validator';
 
 export class LoginDTO {
     @IsEmail()
+    @ApiProperty({ required: false })
     @ValidateIf((obj: LoginDTO) => !obj.username)
     readonly email?: string;
 
     @IsString()
+    @ApiProperty({ required: false })
     @ValidateIf((obj: LoginDTO) => !obj.email)
     readonly username?: string;
 
     @IsString()
     @MinLength(8)
+    @ApiProperty()
     readonly password: string;
 }
--- src/account/dto/register.dto.ts
+++ src/account/dto/register.dto.ts
@@ -1,19 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsOptional, IsString, MinLength } from "class-validator";
 
 export class RegisterDTO {
     @IsEmail()
+    @ApiProperty()
     readonly email?: string;
 
-    @IsString() @IsOptional()
+    @IsString()
+    @IsOptional()
+    @ApiProperty()
     readonly firstName?: string;
 
-    @IsString() @IsOptional()
+    @IsString()
+    @IsOptional()
+    @ApiProperty()
     readonly lastName?: string;
 
-    @IsString() @IsOptional()
+    @IsString()
+    @IsOptional()
+    @ApiProperty()
     readonly username?: string;
 
     @IsString()
     @MinLength(8)
+    @ApiProperty()
     readonly password: string;
 }
